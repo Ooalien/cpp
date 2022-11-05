069135fe67a4642c64562d6895a422188fdc82e3
Ooalien <alibayar750@gmail.com>
2022-11-05 10:44:04 +0100

    day03fixed

--- day03/ex03/ClapTrap.cpp
+++ day03/ex03/ClapTrap.cpp
@@ -2,11 +2,6 @@
 
 ClapTrap::ClapTrap( void )
 {
-    this->name = "";
-    this->hit_point = 10;
-    this->energy_p = 10;
-    this->attack_d = 0;
-    this->max_p = this-> energy_p;
     std::cout << "Default constractor called\n";
 }
 
@@ -16,7 +11,7 @@ ClapTrap::ClapTrap( std::string name )
     this->hit_point = 10;
     this->energy_p = 10;
     this->attack_d = 0;
-    this->max_p = this-> energy_p;
+    this->max_p = this->hit_point;
     std::cout << "Name constractor called\n";
 }
 
@@ -38,29 +33,37 @@ std::string ClapTrap::getname()
 
 void ClapTrap::attack(const std::string& target)
 {
-    std::cout << "ClapTrap " << this->getname() << " attacks " << target << ", causing " << this->attack_d <<  " points of damage!" << std::endl;
+    if (this->energy_p && this->hit_point)
+    {
+        std::cout << "ClapTrap " << this->getname() << " attacks " << target << ", causing " << this->attack_d <<  " points of damage!" << std::endl;
+        this->energy_p--;
+    }
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-    this->energy_p -= amount;
-    if (this->energy_p <= 0)
+    this->hit_point -= amount;
+    if (this->hit_point <= 0)
     {
         std::cout << "ClapTrap " << this->getname() << " take " << amount << " dammage point!" << std::endl;
         std::cout << "He's energy point is 0, and he's dead\n";
-    } 
+    }
     else
         std::cout << "ClapTrap " << this->getname() << " take " << amount << " dammage point!" << std::endl; 
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-    this->energy_p += amount;
-    if (this->energy_p >= this->max_p)
+    if (this->energy_p && this->hit_point)
     {
-        std::cout << "ClapTrap " << this->getname() << " get " << amount << " heal point!" << std::endl;
-        std::cout << "He's energy point is max\n";
-    } 
-    else
-        std::cout << "ClapTrap " << this->getname() << " get " << amount << " heal point!" << std::endl; 
+        this->energy_p--;
+        this->hit_point += amount;
+        if (this->hit_point >= this->max_p)
+        {
+            std::cout << "ClapTrap " << this->getname() << " get " << amount << " heal point!" << std::endl;
+            std::cout << "He's energy point is max\n";
+        } 
+        else
+            std::cout << "ClapTrap " << this->getname() << " get " << amount << " heal point!" << std::endl; 
+    }
 }
